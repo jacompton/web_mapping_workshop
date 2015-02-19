68f379044fcc7f60906a7a4a16695384d01fdf41
jacompton <comptj2@lpha.mopublic.org>
2015-02-19 13:50:11 -0600

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var mapId = 'wjacompton.l8mm66g'; //<- this references the ugly green map that I made
+var token = 'pk.eyJ1IjoiamFjb21wdG9uIiwiYSI6IlJFOXlUNXcifQ.G3sRa4kEZCJVwIZblWVhTg'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
