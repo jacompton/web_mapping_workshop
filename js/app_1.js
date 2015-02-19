b4bc6b2b87e1791504a033070a2d2aaf996ab99a
jacompton <comptj2@lpha.mopublic.org>
2015-02-19 15:53:55 -0600

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -32,3 +32,22 @@ featureLayer.on('ready', function(){
     })
 })
 
+
+var myLocation = L.mapbox.featureLayer().addTo(map);
+map.locate();
+map.on('locationfound', function(e) {
+
+    myLocation.setGeoJSON({
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [e.latlng.lng, e.latlng.lat]
+        },
+        properties: {
+            'title': 'Here I am!',
+            'marker-color': '#ff8888',
+            'marker-symbol': 'star'
+        }
+    });
+});
+map.locate();
